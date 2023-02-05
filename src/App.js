dc9e4429668ae94e2d1654665453f7b4e7f217a4
jitujiten <sahuohjitu@gmail.com>
2023-02-05 10:44:36 +0530

    Added the task-7

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import "./App.css";
 function App() {
   const [movies, setmovies] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+
   async function fetchMoviehandler() {
+    setLoading(true);
     const response = await fetch("https://swapi.dev/api/films");
+
     const data = await response.json();
 
     const transformedmovies = data.results.map((moviedata) => {
@@ -19,6 +23,7 @@ function App() {
       };
     });
     setmovies(transformedmovies);
+    setLoading(false);
   }
 
   return (
@@ -27,7 +32,20 @@ function App() {
         <button onClick={fetchMoviehandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!loading && movies.length>0 &&  <MoviesList movies={movies} />}
+
+        {!loading  && movies.length===0 && (
+          <h2>
+            No Movies Found
+          </h2>
+        )}
+
+        {loading  && movies.length===0 && (
+          <div>
+            <div className="spinner-border text-danger" role="status"></div>
+            <div>Loading...</div>
+          </div>
+        )}
       </section>
     </React.Fragment>
   );
