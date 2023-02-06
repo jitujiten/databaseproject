e32d7a1f150454729a4f5ff4b14bd64d7d9eef48
jitujiten <sahuohjitu@gmail.com>
2023-02-06 12:21:16 +0530

    Added task-10

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { useState, useEffect,useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
-  
   const [movies, setmovies] = useState([]);
 
   const [isLoading, setloading] = useState(false);
 
   const [error, seterror] = useState(null);
 
-
- 
-  
-  const fetchMoviehandler =useCallback(async ()=>{
+  const fetchMoviehandler = useCallback(async () => {
     try {
       setloading(true);
 
@@ -27,30 +23,68 @@ function App() {
       const data = await response.json();
 
       const transformedmovies = data.results.map((moviedata) => {
-          return {
-            id: moviedata.episode_id,
-            title: moviedata.title,
-            openingText: moviedata.opening_crawl,
-            releaseDate: moviedata.release_date,
-          };
-        });
-    
-      
+        return {
+          id: moviedata.episode_id,
+          title: moviedata.title,
+          openingText: moviedata.opening_crawl,
+          releaseDate: moviedata.release_date,
+        };
+      });
+
       setmovies(transformedmovies);
     } catch (error) {
       seterror(error.message);
     }
     setloading(false);
-  },[]);
+  }, []);
 
   useEffect(() => {
     fetchMoviehandler();
-    console.log("fetched")
-}, [fetchMoviehandler]);
+    console.log("fetched");
+  }, [fetchMoviehandler]);
+
+const submitHandler=(e)=>{
+  e.preventDefault();
+
+  const movieObject={
+    "title":e.target.title.value,
+    "opening_crawl":e.target.OpeningText.value,
+    "release_date":new Date(e.target.date.value)
+  }
+ console.log(movieObject);
+
+}
+
+
+
 
 
   return (
     <React.Fragment>
+      <div className="section1">
+        <form onSubmit={submitHandler}>
+          <label htmlFor="Title" className="form-label">
+            Title
+          </label>
+          <input id="title" type="text" className="form-control" />
+          <label htmlFor="Opening Text" className="form-label">
+            Opening Text
+          </label>
+          <textarea
+            id="OpeningText"
+            className="form-control"
+            rows="3"
+          ></textarea>
+          <label htmlFor="Release Date" className="form-label">
+          Release Date
+          </label>
+          <input type="date" 
+           id="date" 
+          className="form-control" />
+          <button>Add More</button>
+        </form>
+      </div>
+
       <section>
         <button onClick={fetchMoviehandler}>Fetch Movies</button>
       </section>
